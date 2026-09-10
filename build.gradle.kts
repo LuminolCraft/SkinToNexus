dd27ccb0bc8687129d65f42de77e428ffdc29e66
NatJerry <xiaomajunqwq123@outlook.com>
2026-09-10 21:34:22 +0800

    chore: Add ktor and kotlinx-coroutines dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,19 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.org/repository/maven-public/") {
+        name = "codemc"
+    }
 }
 
 dependencies {
     compileOnly("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT")
+    compileOnly("net.skinsrestorer:skinsrestorer-api:+")
+    implementation("com.electronwill.night-config:yaml:3.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    implementation("io.ktor:ktor-client-cio:3.5.2")
+    implementation("io.ktor:ktor-client-content-negotiation:3.5.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.5.2")
 }
 
 kotlin {
